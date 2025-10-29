2558a1c5e4d8a79ff1654c6e5ce899e7da2a8714
auroramonet <mii-@tutanota.com>
2025-10-29 19:03:14 -0400

    init

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files
+app.use(express.static(path.join(__dirname)));
+
+// Handle React routing, return all requests to React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`inc.online running on port ${PORT}`);
+  console.log(`Visit: http://localhost:${PORT}`);
+});
+
